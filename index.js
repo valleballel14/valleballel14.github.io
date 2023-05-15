0d5086c6594da945139b2cf944183624e3934ae9
valleballel14 <valentin.hollander@hotmail.com>
2023-05-15 20:58:05 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var hamburger = document.querySelector(".hamburger");
-    var navActive = document.querySelector(".nav-active");
-  
-    hamburger.classList.toggle("change");
-    navActive.classList.toggle("show");
-  }
-  
